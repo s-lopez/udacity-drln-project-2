9e297555f1ef9e8cdd91089203b1b4efc5aa70ea
Samuel López Santamaría <slopez@posteo.eu>
2019-12-27 16:45:55 +0000

    performance tweaks, task solved in 356 steps

--- ddpg_model.py
+++ ddpg_model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=150, fc2_units=75):
+    def __init__(self, state_size, action_size, seed, fc1_units=240, fc2_units=120):
         """Initialize parameters and build model.
         Params
         ======
@@ -45,7 +45,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=150, fc2_units=75):
+    def __init__(self, state_size, action_size, seed, fcs1_units=240, fc2_units=120):
         """Initialize parameters and build model.
         Params
         ======
@@ -58,7 +58,7 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
-        #self.bn1 = nn.BatchNorm1d(num_features=fcs1_units)
+        self.bn1 = nn.BatchNorm1d(num_features=fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
@@ -70,7 +70,7 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        #xs = F.relu(self.bn1(self.fcs1(state)))
+        xs = F.relu(self.bn1(self.fcs1(state)))
         xs = F.relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
